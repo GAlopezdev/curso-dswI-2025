8768eba2f9995639357e87cc2a9b1ec777654364
Gabriel <gabriel.devback@gmail.com>
2025-11-15 14:36:46 -0500

    Agregué método EsPrimo en la clase clsNumero

--- appCore_Ejer01/appCore_Ejer01/Models/clsNumero.cs
+++ appCore_Ejer01/appCore_Ejer01/Models/clsNumero.cs
@@ -10,6 +10,16 @@
         public bool EsPrimoResultado { get; set; }
         public double FactorialResultado { get; set; }
 
-
+        public static bool EsPrimo(double numero)
+        {
+            for (int i = 2; i <= numero/2; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
