54c7c56d4bba2cbb7762425be812c624e6bddebf
Gabriel <gabriel.devback@gmail.com>
2025-11-15 14:30:51 -0500

    creación de la clase clsNumero

---
+++ appCore_Ejer01/appCore_Ejer01/Models/clsNumero.cs
@@ -0,0 +1,15 @@
+﻿namespace appCore_Ejer01.Models
+{
+    public class clsNumero
+    {
+        // Esta clase tiene 2 metodos :
+        // EsPrimo, que determina si un numero es primo o no
+        // calcularFactorial, que calcula el factorial de un numero
+
+        public double Numero { get; set; }
+        public bool EsPrimoResultado { get; set; }
+        public double FactorialResultado { get; set; }
+
+
+    }
+}
