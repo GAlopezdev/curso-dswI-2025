e2a09a4b678ff8aac2a8b01f588636a16823592e
Gabriel <gabriel.devback@gmail.com>
2025-11-15 15:25:28 -0500

    creación del controller NumeroController y vista Index

--- appCore_Ejer01/appCore_Ejer01/Controllers/NumeroController.cs
+++ appCore_Ejer01/appCore_Ejer01/Controllers/NumeroController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using appCore_Ejer01.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace appCore_Ejer01.Controllers
 {
@@ -6,7 +7,25 @@ namespace appCore_Ejer01.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new clsNumero());
+        }
+
+        [HttpPost]
+        public IActionResult Index(clsNumero model)
+        {
+
+            // validación básica para números negativos
+            if (model.Numero < 0)
+            {
+                ModelState.AddModelError("Numero", "Por favor, ingrese un número no negativo.");
+                return View(model);
+            }
+
+            // llama al método para calcular si el número es primo
+            model.EsPrimoResultado = clsNumero.EsPrimo(model.Numero);
+
+            // devuelve la vista con el modelo actualizado
+            return View(model);
         }
     }
 }
