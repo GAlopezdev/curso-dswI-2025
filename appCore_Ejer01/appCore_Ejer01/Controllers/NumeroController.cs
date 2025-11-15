017b560bc0782fce5055ff5a62c98cd3b4cb302b
Gabriel <gabriel.devback@gmail.com>
2025-11-15 15:37:36 -0500

    Implementación de acción CalcularFactorial con validación en NumeroController  y vistas asociadas(CalcularFactorial y ResultadoFactorial)

--- appCore_Ejer01/appCore_Ejer01/Controllers/NumeroController.cs
+++ appCore_Ejer01/appCore_Ejer01/Controllers/NumeroController.cs
@@ -27,5 +27,28 @@ namespace appCore_Ejer01.Controllers
             // devuelve la vista con el modelo actualizado
             return View(model);
         }
+
+        // ===== Acción para calcular el factorial =====
+
+        public IActionResult CalcularFactorial()
+        {
+            return View(new clsNumero());
+        }
+
+        [HttpPost]
+        public IActionResult CalcularFactorial(clsNumero model)
+        {
+            // validación básica para números negativos
+            if (model.Numero < 0)
+            {
+                ModelState.AddModelError("Numero", "Por favor, ingrese un número no negativo.");
+                return View("Factorial", model);
+            }
+            // llama al método para calcular el factorial
+            model.FactorialResultado = clsNumero.CalcularFactorial(model.Numero);
+
+            // devuelve la vista con el modelo actualizado
+            return View("ResultadoFactorial", model);
+        }
     }
 }
