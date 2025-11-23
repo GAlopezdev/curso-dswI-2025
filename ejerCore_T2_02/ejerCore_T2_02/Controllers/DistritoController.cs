b41a02497b9324023d80f8a6fe06470dd7d15d8f
Gabriel <gabriel.devback@gmail.com>
2025-11-23 14:30:45 -0500

    Agrega metodo y vista para listar distritos

--- ejerCore_T2_02/ejerCore_T2_02/Controllers/DistritoController.cs
+++ ejerCore_T2_02/ejerCore_T2_02/Controllers/DistritoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ejerCore_T2_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ejerCore_T2_02.Controllers
 {
@@ -11,9 +13,32 @@ namespace ejerCore_T2_02.Controllers
             _config = config;
         }
 
-        public IActionResult Index()
+        // Metodo para obtener el listado de distritos desde la base de datos
+        public IEnumerable<Distrito> ListadoDistrito()
+        { 
+            List<Distrito> temporal = new List<Distrito>();
+
+            using (SqlConnection cn = new SqlConnection(_config.GetConnectionString("Infracciones")))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("usp_ListarDistrito", cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    temporal.Add(new Distrito()
+                    {
+                        Cod_distrito = dr.GetString(0),
+                        Nom_distrito = dr.GetString(1)
+                    });
+                }
+                dr.Close();
+            }
+            return temporal;
+        }
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await Task.Run(() => ListadoDistrito()));
         }
     }
 }
