c481190d161410fb57f06a61c1984fdc95b60623
Gabriel <gabriel.devback@gmail.com>
2025-11-23 14:16:37 -0500

    Agrega Controlador Distrito

---
+++ ejerCore_T2_02/ejerCore_T2_02/Controllers/DistritoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejerCore_T2_02.Controllers
+{
+    public class DistritoController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public DistritoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
