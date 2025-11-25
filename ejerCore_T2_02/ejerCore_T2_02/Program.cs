8cb3ba4065df534cc4f7cdd5859510d528930f25
Gabriel <gabriel.devback@gmail.com>
2025-11-24 19:33:45 -0500

    Cambia el default route a PropietarioController

--- ejerCore_T2_02/ejerCore_T2_02/Program.cs
+++ ejerCore_T2_02/ejerCore_T2_02/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Propietario}/{action=Index}/{id?}");
 
 app.Run();
